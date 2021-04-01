7b0003e10ca6f9c68c4f3ee38cb2203e951dd913
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-01 16:06:32 +0600

    set routing for AdminModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminModuleComponent} from "./admin-module/admin-module.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: "admin", component: AdminModuleComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
