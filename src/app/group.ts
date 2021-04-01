53a441a847f3ca308ad733c90b9c6d328b210a59
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-01 16:04:28 +0600

    adding group.ts

---
+++ src/app/group.ts
@@ -0,0 +1,9 @@
+export class Group {
+  id: string;
+  year_of_study: number;
+
+  constructor(id: string, year_of_study: number) {
+    this.id = id;
+    this.year_of_study = year_of_study;
+  }
+}
